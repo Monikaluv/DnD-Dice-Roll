d259732353fc71c018ee557e6e1e7d05543e3858
Monikaluv <vtonnicchi2006@gmail.com>
2025-05-22 15:05:29 +0200

    there's a window now (●'◡'●)

---
+++ src/Window/Window.java
@@ -0,0 +1,11 @@
+package Window;
+import javax.swing.JFrame;
+
+public class Window
+{
+    public Window()
+    {
+        JFrame window = new JFrame("DnD Dice Roll");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
