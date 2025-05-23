b34f6466b703ea98d3bb9e6bd671ec4a4347f5e0
Monikaluv <vtonnicchi2006@gmail.com>
2025-05-23 14:36:04 +0200

    WINDOW EXISTS WITH BUTTONS (they dont work)

--- src/Window/Window.java
+++ src/Window/Window.java
@@ -1,11 +1,68 @@
 package Window;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Window
 {
+    JFrame window;
+    LayoutManager grid;
+
     public Window()
     {
-        JFrame window = new JFrame("DnD Dice Roll");
+        window = new JFrame("DnD Dice Roll");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+        setup(5,5);
+    }
+
+    public void setup(int i, int j)
+    {
+        grid = new GridLayout(i,j);
+        window.setLayout(grid);
+        JButton diceButton = new JButton();
+        JButton arrButton = new JButton();
+        JButton exitButton = new JButton();
+
+        diceButton.setText("Die");
+        arrButton.setText("Manual Array");
+        exitButton.setText("Exit");
+        
+        JPanel[][] panelholder = new JPanel[i][j];
+
+        for(int m = 0; m < i; m++)
+        {
+            for(int n = 0; n < j; n++)
+            {
+                panelholder[m][n] = new JPanel();
+                window.add(panelholder[m][n]);
+            }
+        }
+        panelholder[1][0].add(diceButton);
+        panelholder[1][4].add(arrButton);
+        panelholder[2][2].add(exitButton);
+
+        window.setSize(500, 500);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+
+    public void DiceScreen()
+    {
+
+    }
+
+    public void ArrayScreen()
+    {
+
+    }
+
+    public void exit()
+    {
+        window.dispose();
     }
 }
